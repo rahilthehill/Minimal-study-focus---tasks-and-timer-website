e5f3d899b5495f328844b9a89c33252985f7c415
rahilthehill <69742190+rahilthehill@users.noreply.github.com>
2023-04-27 01:10:28 +0800

    start page, mobile styling, smoother, more details

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-const taskList = [ // task list array, all timing is in mins
-    { task: "Pomodoro 1 - Paper 1 Questions", duration: 25 },
-    { task: "Short Break", duration: 10 },
-    { task: "Pomodoro 2 - Paper 1 Questions", duration: 25 },
-    { task: "Short Break", duration: 10 },
-    { task: "Pomodoro 3 - Paper 1 Questions", duration: 25 },
-    { task: "Long Break (Lunch)", duration: 55 },
-    { task: "Pomodoro 4 - Paper 2", duration: 60 },
-    { task: "Short Break", duration: 15 },
-    { task: "Pomodoro 5 - Paper 3", duration: 60 },
-    { task: "Short Break", duration: 15 },
-    { task: "Pomodoro 6 - Paper 1 Questions", duration: 25 },
-    { task: "Short Break", duration: 10 },
-    { task: "Pomodoro 7 - Paper 1 Questions", duration: 25 },
-    { task: "Short Break", duration: 10 },
-    { task: "Pomodoro 8 - Review", duration: 25 },
-    { task: "Personal Time", duration: 25 },
-  ];
+// const taskList = [ // task list array, all timing is in mins
+//     { task: "Pomodoro 1 - Paper 1 Questions", duration: 25 },
+//     { task: "Short Break", duration: 10 },
+//     { task: "Pomodoro 2 - Paper 1 Questions", duration: 25 },
+//     { task: "Short Break", duration: 10 },
+//     { task: "Pomodoro 3 - Paper 1 Questions", duration: 25 },
+//     { task: "Long Break (Lunch)", duration: 55 },
+//     { task: "Pomodoro 4 - Paper 2", duration: 60 },
+//     { task: "Short Break", duration: 15 },
+//     { task: "Pomodoro 5 - Paper 3", duration: 60 },
+//     { task: "Short Break", duration: 15 },
+//     { task: "Pomodoro 6 - Paper 1 Questions", duration: 25 },
+//     { task: "Short Break", duration: 10 },
+//     { task: "Pomodoro 7 - Paper 1 Questions", duration: 25 },
+//     { task: "Short Break", duration: 10 },
+//     { task: "Pomodoro 8 - Review", duration: 25 },
+//     { task: "Personal Time", duration: 25 },
+//   ];
   
+const taskList = [
+  { task: "Pomodoro 1 - Paper 1 Questions - Focus on answering 10 paper 1 questions with no distractions", duration: 25 },
+  { task: "Short Break - Take a 10-minute break to rest, stretch, or grab a snack", duration: 10 },
+  { task: "Pomodoro 2 - Paper 1 Questions - Focus on answering another 10 paper 1 questions with no distractions", duration: 25 },
+  { task: "Short Break - Take a 10-minute break to rest, stretch, or do something you enjoy", duration: 10 },
+  { task: "Pomodoro 3 - Paper 1 Questions - Focus on answering another 10 paper 1 questions with no distractions", duration: 25 },
+  { task: "Short Break - Take a 15-minute break to rest, stretch, or do something you enjoy", duration: 15 },
+  { task: "Pomodoro 4 - Revision/Paper 3 Questions - Focus on answering as many questions as possible within the hour, with no distractions", duration: 60 },
+  { task: "Short Break - Take a 10-minute break to rest, stretch, or do something you enjoy", duration: 10 },
+  { task: "Pomodoro 5 - Revision/Paper 3 Questions - Focus on answering as many questions as possible within the 30 minutes, with no distractions", duration: 30 },
+  { task: "Personal Time - Use this time to watch a show or YouTube video, relax, or do something you enjoy", duration: 30 },
+];
+
   //index 1
   let taskIndex = 0;
   let startTime;
@@ -53,15 +66,18 @@ const taskList = [ // task list array, all timing is in mins
   }
   
   //shows clock in the corner of real time
-    function displayClock() {
-        const clock = document.getElementById("clock");
-        const now = new Date();
-        const hours = now.getHours().toString().padStart(2, "0");
-        const minutes = now.getMinutes().toString().padStart(2, "0");
-        const seconds = now.getSeconds().toString().padStart(2, "0");
-        const time = hours + ":" + minutes + ":" + seconds;
-        clock.textContent = time;
-      }
+  function displayClock() {
+    const clock = document.getElementById("clock");
+    const now = new Date();
+    let hours = now.getHours();
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+    const amOrPm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    const time = hours + ":" + minutes + ":" + seconds + " " + amOrPm;
+    clock.textContent = time;
+  }
+  
       
       //when skip pressed or time elapsed it goes to next task
       function displayNextTask() {
@@ -73,6 +89,7 @@ const taskList = [ // task list array, all timing is in mins
         } else {
           displayTask({ task: "You're done!", duration: 0 });
           document.getElementById("timer").textContent = "00:00";
+          document.body.style.backgroundColor = "#0f0";
         }
       }
       
@@ -88,22 +105,13 @@ const taskList = [ // task list array, all timing is in mins
           playBeep()
           clearInterval(timerId);
           displayNextTask();
-          document.body.style.backgroundColor = "#0f0";
+          document.body.style.backgroundColor = "#06b33a";
           setTimeout(() => {
             document.body.style.backgroundColor = "#000";
           }, 500);
         });
       }
-      
-      function hide() {
-        //hide element h2
-        document.getElementById("h2");
-        if (h2.style.opacity === "0") {
-          h2.style.opacity = "1";
-        } else {
-          h2.style.display = "0";
-      }
-    }
+    
 
     function toggleFullScreen() {
       if (!document.fullscreenElement) {
@@ -120,6 +128,20 @@ const taskList = [ // task list array, all timing is in mins
       audio.play();
     }
     
+    function reloadPage() {
+      // window.location.href = window.location.href;
+      window.location.href = "index.html";
+    }
+    
+    
+    function hide() {
+      const about = document.getElementById("about");
+      about.style.display = "none";
+    }
+
+    function loadMain() {
+      window.location.href = "main.html";
+    }
     
       init();
       
